d9b24d286d8778dc31828228b7d05dc82966966b
Nancy C. Harris <35252122+nanzycharris@users.noreply.github.com>
2019-12-27 03:09:20 -0600

    Update bamazoncustomer.js

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "pearsley03",
+    password: " ",
     database: "bamazonDB"
 });
 
